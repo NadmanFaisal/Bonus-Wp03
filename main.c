a920d0df55c15a216bd2ecaf01cc649a786737bf
Nadman Faisal <nadman@chalmers.se>
2025-02-04 17:41:45 +0100

    Take inputs for dynamic matrices sizes

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define MAX_SIZE 4;
 
 // This method clears buffer
@@ -7,17 +9,37 @@ void clearBuffer (void) {
     while ( (input = getchar()) != '\n' && input != EOF );
 }
 
+void takeInputs(int *firstMatrixSize, int *secondMatrixSize) {
+    char temp[200];
+    printf("Input the size: ");
+    fgets(temp, sizeof(temp), stdin);
+
+
+    char *token = strtok(temp, " ");
+
+    for(int i = 0; i < 2; i++) {
+        if(i == 0) {
+            *firstMatrixSize = atoi(token);
+        } else {
+            *secondMatrixSize = atoi(token);
+        }
+        token = strtok(NULL, " ");
+    }
+}
+
 int main(int argc, char *argv[]) {
     int size_matrix_1;
     int size_matrix_2;
-
     int *pSize_matrix_1 = &size_matrix_1;
     int *pSize_matrix_2 = &size_matrix_2;
 
-    printf("Input the size: ");
-    scanf("%d %d", pSize_matrix_1, pSize_matrix_2);
+    char stringMatrix1[200];
+    char stringMatrix2[200];
 
-    int matrix_1[*pSize_matrix_1][*pSize_matrix_2];
+    takeInputs(pSize_matrix_1, pSize_matrix_2);
 
-    printf("Size 1: %d, size 2: %d\n", size_matrix_1, size_matrix_2);
+    int matrix_1[2][2];
+    int matrix_2[2][2];
+
+
 }
