0114334d76a9e26eaa44ac11f5488de536153bb8
Nadman Faisal <nadman@chalmers.se>
2025-02-04 20:13:42 +0100

    CAnnot set the values of the array fsr

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_SIZE 4;
+#define MAX_SIZE 4
 
 // This method clears buffer
 void clearBuffer (void) {
@@ -9,11 +9,35 @@ void clearBuffer (void) {
     while ( (input = getchar()) != '\n' && input != EOF );
 }
 
-void takeInputs(int *firstMatrixSize, int *secondMatrixSize) {
+void setMatrixValues(int *firstMatrixSize, int **firstMatrix) {
+    char temp2[200];
+    int i;
+    int j;
+    int *pI = &i;
+    int *pJ = &j;
+
+    // printf("First matrix size: %d\n", firstMatrixSize);
+
+    printf("Input elements of matrix 1: ");
+    fgets(temp2, sizeof(temp2), stdin);
+    char *token = strtok(temp2, " ");
+
+    for(*pI = 0; *pI < firstMatrixSize; ++*pI) {
+        for(*pJ = 0; *pJ < firstMatrixSize; ++*pJ) {
+            printf("I: %d, J: %d\n", *pI, *pJ);
+            printf("Token: %d\n", atoi(token));
+            firstMatrix[*pI][*pJ] = atoi(token);
+            printf("Value: %d\n", firstMatrix[*pI][*pJ]);
+            token = strtok(NULL, " ");
+        }
+    }
+
+}
+
+void setMatrixSize(int *firstMatrixSize, int *secondMatrixSize) {
     char temp[200];
     printf("Input the size: ");
     fgets(temp, sizeof(temp), stdin);
-
 
     char *token = strtok(temp, " ");
 
@@ -36,10 +60,12 @@ int main(int argc, char *argv[]) {
     char stringMatrix1[200];
     char stringMatrix2[200];
 
-    takeInputs(pSize_matrix_1, pSize_matrix_2);
+    setMatrixSize(pSize_matrix_1, pSize_matrix_2);
 
-    int matrix_1[2][2];
-    int matrix_2[2][2];
+    int matrix_1[*pSize_matrix_1][*pSize_matrix_1];
+    int matrix_2[*pSize_matrix_2][*pSize_matrix_2];
 
+    printf("Size of matrix 1: %d\n", *pSize_matrix_1);
+    setMatrixValues(*pSize_matrix_1, matrix_1);
 
 }
